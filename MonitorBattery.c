1b461249091582085348638bce56294d5cd7306d
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-02-08 18:57:35 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -1,25 +1,14 @@
 #include "BatteryStatusLib.h"
 #include "MonitorBattery.h"
 
-bool batteryIsOk(BMSParameters BatteryParameters) {
+bool batteryIsOk(BMSParameters BatteryParameters) 
+{
   bool BatteryStatus = BATTERY_HEALTH_IS_GOOD;
 
-  if(!BatteryTemperatureIsGood(BatteryParameters.Temperature))
-  {
-      BatteryStatus = BATTERY_HEALTH_IS_NOT_GOOD;
-  }
-  if(!BatteryCapacityIsGood(BatteryParameters.SoC))
-  {
-      BatteryStatus = BATTERY_HEALTH_IS_NOT_GOOD;
-  }
-  if(!BatteryChargingAtGoodRate(BatteryParameters.ChargingRate))
-  {
-      BatteryStatus = BATTERY_HEALTH_IS_NOT_GOOD;
-  }
-  if(!BatteryDischargingAtGoodRate(BatteryParameters.DischargingRate))
-  {
-      BatteryStatus = BATTERY_HEALTH_IS_NOT_GOOD;
-  }
+  BatteryStatus &&= BatteryTemperatureIsGood(BatteryParameters.Temperature);
+  BatteryStatus &&= BatteryCapacityIsGood(BatteryParameters.SoC);
+  BatteryStatus &&= BatteryChargingAtGoodRate(BatteryParameters.ChargingRate);
+  BatteryStatus &&= BatteryDischargingAtGoodRate(BatteryParameters.DischargingRate);
 
   return BatteryStatus;
 }
