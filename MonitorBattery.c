a38c67962aa1b5087512ee925c512078dcdf864b
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-02-08 19:19:36 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -5,7 +5,7 @@ bool batteryIsOk(BMSParameters BatteryParameters)
 {
   bool BatteryStatus = BATTERY_HEALTH_IS_GOOD;
 
-  BatteryStatus &&= BatteryTemperatureIsGood(BatteryParameters.Temperature);
+  BatteryStatus &&= BatteryTemperatureIsGood(BatteryParameters.ChargingTemperature, BatteryParameters.DischargingTemperature);
   BatteryStatus &&= BatteryCapacityIsGood(BatteryParameters.SoC);
   BatteryStatus &&= BatteryChargingAtGoodRate(BatteryParameters.ChargingRate);
   BatteryStatus &&= BatteryDischargingAtGoodRate(BatteryParameters.DischargingRate);
