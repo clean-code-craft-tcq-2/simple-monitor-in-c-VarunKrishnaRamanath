eea6efd744801be5e98ada7f344db823bcff4e4d
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-02-08 20:09:39 +0530

    Update MonitorBattery.h

--- MonitorBattery.h
+++ MonitorBattery.h
@@ -8,7 +8,6 @@
 extern bool batteryIsOk(BMSParameters BatteryParameters);
 extern bool BatteryTemperatureIsGood(float chargingTemperature, float dischargingTemperature);
 extern bool BatteryCapacityIsGood(float SoC);
-extern bool BatteryChargingAtGoodRate(float ChargeRate);
-extern bool BatteryDischargingAtGoodRate(float DischargeRate);
+extern bool ChargeAndDischargeIsGood(float ChargeRate, float DischargeRate);
 
 #endif // MONITORBATTERY_H_INCLUDED
