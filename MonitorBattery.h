9f706b76bfca9d3634d29765a412d1edbecc82bd
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-02-08 19:55:50 +0530

    Update MonitorBattery.h

--- MonitorBattery.h
+++ MonitorBattery.h
@@ -1,6 +1,8 @@
 #ifndef MONITORBATTERY_H_INCLUDED
 #define MONITORBATTERY_H_INCLUDED
 
+#include "BatteryStatusLib.h"
+
 #define BATTERY_TYPE    LITHIUM_ION
 
 extern bool batteryIsOk(BMSParameters BatteryParameters);
